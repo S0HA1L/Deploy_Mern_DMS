2116185bc1ea3d7b6410ba8624b9802a870f6999
S0HA1L <64415891+S0HA1L@users.noreply.github.com>
2023-08-02 12:53:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,13 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express()
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-dms-api.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-))
+app.use(cors())
 app.use(express.json())
 
 const PORT  = process.env.PORT || 8080 
